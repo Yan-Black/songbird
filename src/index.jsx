d2bee8d50a02e04c9eba3452b0b1c61a1d14ff58
Yan Andryshkin <yan.7feb@gmail.com>
2020-08-02 19:38:02 +0300

    build: create object structure, add sass, add webpack config

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import View from './components/View';
+
+render(<View />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: './public/index.html',
+  filename: './index.html',
+});
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, loader: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(svg|png|jpe?g)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+};
